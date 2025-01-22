949a310ca92e6372f0b4ba7c7c2e9837005abd29
Abi-ezer Guerra <axiever.guejia@gmail.com>
2025-01-22 14:03:24 -0500

    02_types

---
+++ 01_basic/02_types.py
@@ -0,0 +1,15 @@
+print("int:", 5)
+print(type(12))
+
+print("float:", 9.5)
+print(type(3.14))
+
+print("complex:", 1 + 2j)
+print(type(1 + 2j))
+
+print("bool:", True, False)
+print(type(True))
+print("bool:", 2>1)
+
+print("NoneType:", None)
+print(type(None))
